1b0a914af303cc195e7da11b7556a902d96ab894
Krisztian Holub <kholub1989@gmail.com>
2020-09-12 23:57:09 +0300

    add environment variables

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
 
-const port = 3001;
+const port = process.env.PORT || 8081;
 app.listen(port, () => {
   console.log(`Server is 🚀 and listening on port ${port}`);
 });
