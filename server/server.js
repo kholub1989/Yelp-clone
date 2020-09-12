b4e6740f0859736ad34958a010e7a6363367e515
Krisztian Holub <kholub1989@gmail.com>
2020-09-13 00:13:56 +0300

    add first express route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,13 @@ const express = require("express");
 
 const app = express();
 
+app.get("/getRestaurants", (req, res) => {
+  res.json({
+    status: "success",
+    restaurant: "mcdonalds",
+  });
+});
+
 const port = process.env.PORT || 8081;
 app.listen(port, () => {
   console.log(`Server is 🚀 and listening on port ${port}`);
