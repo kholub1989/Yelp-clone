23dcff3b9afb59af848e120b1712ec5806d0db06
Krisztian Holub <kholub1989@gmail.com>
2020-09-21 13:13:03 -0400

    add review

--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import RestaurantFinder from "../apis/RestaurantFinder";
+import AddReview from "../components/AddReview";
 import Reviews from "../components/Reviews";
 import StarRating from "../components/StarRating";
 import { RestaurantsContext } from "../context/RestaurantsContext";
@@ -14,7 +15,7 @@ const RestaurantDetailPage = () => {
     const fetchData = async () => {
       try {
         const response = await RestaurantFinder.get(`/${id}`);
-        setSelectedRestaurant(response.data.data.restaurant);
+        setSelectedRestaurant(response.data.data);
       } catch (error) {
         console.log(error);
       }
@@ -25,9 +26,13 @@ const RestaurantDetailPage = () => {
     <div>
       {selectedRestaurant && (
         <>
+          <h1 className="text-center display-1">
+            {selectedRestaurant.restaurant.name}
+          </h1>
           <div className="mt-3">
-            <Reviews />
+            <Reviews reviews={selectedRestaurant.reviews} />
           </div>
+          <AddReview />
         </>
       )}
     </div>
